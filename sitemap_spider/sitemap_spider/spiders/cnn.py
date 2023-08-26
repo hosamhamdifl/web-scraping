dc64e4e5e588dbc8fc2bd15d37506c1fe9f4f8b1
hosamhamdifl <hosamhamdifreelance@gmail.com>
2023-08-27 00:29:45 +0300

    sitemap spider

---
+++ sitemap_spider/sitemap_spider/spiders/cnn.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule, SitemapSpider
+from scrapy.linkextractors import LinkExtractor
+from sitemap_spider.items import NewsCrawlerItem
+
+
+class CnnSpider(SitemapSpider):
+    name = 'cnn'
+    allowed_domains = ['cnn.com']
+    sitemap_urls = ['https://www.cnn.com/sitemaps/article-2020-10.xml']
+
+    def parse(self, response):
+        article = NewsCrawlerItem()
+        # <script data-rh="true">
+        article['url'] = response.url
+        article['source'] = 'CNN'
+        article['title'] = response.xpath('//h1/text()').get()
+        article['description'] = response.xpath(
+            '//meta[@name="description"]/@content').get()
+        article['date'] = response.xpath(
+            '//meta[@itemprop="datePublished"]/@content').get()
+        article['author'] = response.xpath(
+            '//meta[@itemprop="author"]/@content').get().replace(', CNN', '')
+        article['text'] = response.xpath(
+            '//section[@data-zone-label="bodyText"]/div[@class="l-container"]//*/text()').getall()
+        return article
