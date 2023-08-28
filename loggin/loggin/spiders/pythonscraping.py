9a2e840b9414938ea426ea0ad75c92febda4a90a
hosamhamdifl <hosamhamdifreelance@gmail.com>
2023-08-28 17:08:51 +0300

    loggin

---
+++ loggin/loggin/spiders/pythonscraping.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class PythonscrapingSpider(scrapy.Spider):
+    name = 'pythonscraping'
+    allowed_domains = ['pythonscraping.com']
+    start_urls = ['http://pythonscraping.com/linkedin/cookies/profile.php']
+
+
+    def make_requests_from_url(self, url):
+        request = super(PythonscrapingSpider, self).make_requests_from_url(url)
+        request.cookies['loggedin'] = 1
+        request.cookies['username'] = 123
+        
+        return request
+
+    def parse(self, response):
+        return { 'text': response.xpath('//body/text()').get() }
